5ac0f5f8b350067902a3c645f9af6574182d261e
bilal <bilal@BILALJAVED>
2024-05-13 16:41:34 +0500

    use configureStore method instead of createStore method

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+// import { rootReducer } from "./reducers";
+
+// const store = createStore(rootReducer,
+//     // in production we need to comment this
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// export default store;
+
+// ! refactored code with configureStore
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers";
 
-//  reating store 
-const store = createStore(rootReducer,
-    // in production we need to comment this 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
 
 export default store;
